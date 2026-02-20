586a051060e0eb2bbac8bc2d6f81007c3776d0b7
danielkrafzik <daniel.krafzik@hotmail.de>
2026-02-20 23:42:42 +0100

    implemented about-me section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, StartScreenComponent],
+  imports: [RouterOutlet, HeaderComponent, StartScreenComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
