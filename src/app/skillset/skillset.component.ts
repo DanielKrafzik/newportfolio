ed0c44fa35d593a0b97d26420c14d0b503d64c37
danielkrafzik <daniel.krafzik@hotmail.de>
2026-03-06 14:13:05 +0100

    implemented tech stack

--- src/app/skillset/skillset.component.ts
+++ src/app/skillset/skillset.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-skillset',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skillset.component.html',
   styleUrl: './skillset.component.scss'
 })
 export class SkillsetComponent {
-
+  skills = [
+    { name: 'HTML', icon: '/img/skill1.png' },
+    { name: 'CSS', icon: '/img/skill2.png' },
+    { name: 'JavaScript', icon: '/img/skill3.png' },
+    { name: 'TypeScript', icon: '/img/skill4.png' },
+    { name: 'Angular', icon: '/img/skill5.png' },
+    { name: 'Firebase', icon: '/img/skill6.png' },
+    { name: 'Git', icon: '/img/skill7.png' },
+    { name: 'REST-API', icon: '/img/skill8.png' },
+    { name: 'Scrum', icon: '/img/skill9.png' },
+    { name: 'Material Design', icon: '/img/skill10.png' }
+  ];
 }
