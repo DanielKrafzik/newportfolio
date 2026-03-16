f98b88cd5df0f70de81e3ba1511c27be88a83b84
danielkrafzik <daniel.krafzik@hotmail.de>
2026-03-16 19:26:53 +0100

    overworked start screen scss

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-start-screen',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
-
+titleUp = 'Frontend';
+titleDown = 'DEVELOPER';
+letters1 = this.titleUp.split('');
+letters2 = this.titleDown.split('');
+hover = false;
 }
