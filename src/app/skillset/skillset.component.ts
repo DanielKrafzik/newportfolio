6f2ea3c98a847072d8d25a4f6f85759678e9a7cd
danielkrafzik <daniel.krafzik@hotmail.de>
2026-03-22 21:21:16 +0100

    finished skillset component

--- src/app/skillset/skillset.component.ts
+++ src/app/skillset/skillset.component.ts
@@ -8,16 +8,34 @@ import { NgFor } from '@angular/common';
   styleUrl: './skillset.component.scss'
 })
 export class SkillsetComponent {
+  hoverIndex: number | null = null;
   skills = [
-    { name: 'HTML', icon: '/img/skill1.png' },
-    { name: 'CSS', icon: '/img/skill2.png' },
-    { name: 'JavaScript', icon: '/img/skill3.png' },
-    { name: 'TypeScript', icon: '/img/skill4.png' },
-    { name: 'Angular', icon: '/img/skill5.png' },
-    { name: 'Firebase', icon: '/img/skill6.png' },
-    { name: 'Git', icon: '/img/skill7.png' },
-    { name: 'REST-API', icon: '/img/skill8.png' },
-    { name: 'Scrum', icon: '/img/skill9.png' },
-    { name: 'Material Design', icon: '/img/skill10.png' }
+    { name: 'HTML', icon: '/img/skill1.png', icon2: '/img/skill1_2.png' },
+    { name: 'CSS', icon: '/img/skill2.png', icon2: '/img/skill2_2.png' },
+    { name: 'JavaScript', icon: '/img/skill3.png', icon2: '/img/skill3_2.png' },
+    { name: 'TypeScript', icon: '/img/skill4.png', icon2: '/img/skill4_2.png' },
+    { name: 'Angular', icon: '/img/skill5.png', icon2: '/img/skill5_2.png' },
+    { name: 'Firebase', icon: '/img/skill6.png', icon2: '/img/skill6_2.png' },
+    { name: 'Git', icon: '/img/skill7.png', icon2: '/img/skill7_2.png' },
+    { name: 'REST-API', icon: '/img/skill8.png', icon2: '/img/skill8_2.png' },
+    { name: 'Scrum', icon: '/img/skill9.png', icon2: '/img/skill9_2.png' },
+    { name: 'Material Design', icon: '/img/skill10.png', icon2: '/img/skill10.png' }
   ];
+  
+  currentPeel = '/img/peel1.png';
+
+  onPeelClick() {
+    if (this.currentPeel.includes('peel3')) {
+      this.currentPeel = '/img/peel1.png';
+      return;
+    }
+
+    if (this.currentPeel.includes('peel1')) {
+      this.currentPeel = '/img/peel2.png';
+
+      setTimeout(() => {
+        this.currentPeel = '/img/peel3.png';
+      }, 200);
+    }
+  }
 }
