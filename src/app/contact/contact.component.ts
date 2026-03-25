d6b16e9b726c3fe2c0211ab186a8417725492c1b
danielkrafzik <daniel.krafzik@hotmail.de>
2026-03-25 21:32:32 +0100

    started form logic

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [FooterComponent],
+  imports: [FooterComponent, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -22,6 +23,21 @@ export class ContactComponent {
       this.boxPath = '/img/checkmark_empty.png';
       this.boxPathHover = '/img/checkmark_empty_hover.png';
       this.boxChecked = false;
+    }
+  }
+
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  onSubmit(form: any) {
+    if(this.boxChecked) {
+      console.log(this.contactData);
+      // Here you can add your form submission logic, e.g., sending the data to a server
+    } else {
+      alert('Please agree to the terms and conditions before submitting the form.');
     }
   }
 }
