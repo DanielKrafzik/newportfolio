9eabf46e7e8202c5c60517ab8e5bbfbb0d20d56c
danielkrafzik <daniel.krafzik@hotmail.de>
2026-03-29 12:00:41 +0200

    complemented contact logic

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -26,18 +27,43 @@ export class ContactComponent {
     }
   }
 
+  http = inject(HttpClient);
+
   contactData = {
     name: '',
     email: '',
     message: ''
   };
 
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if(ngForm.valid && ngForm.submitted && this.boxChecked) {
-      console.log(this.contactData);
-      // Here you can add your form submission logic, e.g., sending the data to a server
-    } else {
-      alert('Please agree to the terms and conditions before submitting the form.');
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 }
