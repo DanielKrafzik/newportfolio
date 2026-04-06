23b4f3f7fb2d3f64c390fa0b0f48814971f9974b
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-06 19:47:54 +0200

    finished styling project details component

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-detail',
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, RouterLink],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.scss'
 })
@@ -22,7 +23,9 @@ export class ProjectDetailComponent {
       description: 'Task manager inspired by the kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       workflow: 'Working in a team environment, I gained experience in collaborative development, version control, and clear communication. I contributed to implementing core features, ensuring clean code structure, and maintaining a user-friendly design. This project strengthened my ability to work both independently on assigned tasks and collaboratively within a development team.',
       duration: '5 weeks',
-      technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase']
+      technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      next: 'sharkie',
+      github: 'https://github.com/DanielKrafzik/join'
     },
     {
       id: 'sharkie',
@@ -31,7 +34,9 @@ export class ProjectDetailComponent {
       description: 'A fun shark-themed game where players navigate through the ocean to collect treasures and defeat the endboss.',
       workflow: 'During development, I focused on implementing game logic, handling user input, and creating smooth animations. This project demonstrates my ability to work independently, think logically, and translate ideas into interactive experiences using modern web technologies.',
       duration: '3 weeks',
-      technologies: ['JavaScript', 'HTML', 'CSS']
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+      next: 'pokedex',
+      github: 'https://github.com/DanielKrafzik/El-pollo-Loco'
     },
     {
       id: 'pokedex',
@@ -40,7 +45,9 @@ export class ProjectDetailComponent {
       description: 'A comprehensive database of all Pokémon species, featuring detailed information, stats, and evolutionary chains.',
       workflow: 'I focused on building reusable components, handling asynchronous data, and ensuring a smooth user experience. This project highlights my ability to work independently, structure complex data, and create clean, maintainable frontend solutions.',
       duration: '2 weeks',
-      technologies: ['JavaScript', 'HTML', 'CSS']
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+      next: 'join',
+      github: 'https://github.com/DanielKrafzik/PokedexV2'
     }
   ];
 
