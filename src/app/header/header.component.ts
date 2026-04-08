95287d426c8f952decdb92c448921397d8b00a37
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-08 20:44:50 +0200

    translated header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  currentLang: 'EN' | 'DE' = 'EN';
+  currentLang: 'en' | 'de' = 'en';
   currentLangIcon = '/img/toggle_en.png';
 
   constructor(private translate: TranslateService) {
-  this.translate.setDefaultLang('en');
-}
+    this.translate.setDefaultLang('en');
+  }
 
-switchLang(lang: string) {
-  this.translate.use(lang);
-}
+  switchLang(): void {
+    this.currentLang = this.currentLang === 'en' ? 'de' : 'en';
+    this.translate.use(this.currentLang);
+    this.toggleLangIcon(this.currentLang);
+  }
+
+  toggleLangIcon(lang: 'en' | 'de'): void {
+    this.currentLangIcon = lang === 'en' ? '/img/toggle_en.png' : '/img/toggle_de.png';
+  }
 }
