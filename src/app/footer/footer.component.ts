13acd1962a710d35347b576ecd956bc350e7510c
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-08 13:55:08 +0200

    finished legal notice

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
+
+
 export class FooterComponent {
 hover = 0;
+
+
+@Input() theme: 'light' | 'dark' = 'light';
 }
