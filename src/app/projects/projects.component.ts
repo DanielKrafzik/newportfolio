935c7c099926e73bc6f1bb6f46ba09bfe4e2e3ea
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-09 23:17:01 +0200

    translated projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink, TranslateModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
