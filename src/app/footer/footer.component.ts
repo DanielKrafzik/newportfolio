44f12fda7298586758fdab67b8e2290bcbf83240
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-09 23:10:30 +0200

    translated footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Input } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink, NgClass, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
