a9601f62b365ae94d29938ba47381df859e59fca
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-10 16:06:18 +0200

    translated legal notice component

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [FooterComponent],
+  imports: [FooterComponent, TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
