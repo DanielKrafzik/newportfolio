1a1a5fb79700a5dfefd776b54876f740c7c0cb97
danielkrafzik <daniel.krafzik@hotmail.de>
2026-04-15 18:44:29 +0200

    implemented firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'portfolio', component: ProjectsComponent },
   { path: 'portfolio/:id', component: ProjectDetailComponent }
 ];
